f168489f8491c9f12defe8e8085d75d4a8dcd847
thangadurai.rajamanickam <thangadurai.rajamanickam@gmail.com>
2025-06-14 09:28:45 +0530

    second commit

---
+++ Test2.cpp
@@ -0,0 +1,14 @@
+// Necessary header files for input output functions
+#include <iostream>
+using namespace std;
+
+// main() function: where the execution of
+// C++ program begins
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+
+    return 0;
+}
